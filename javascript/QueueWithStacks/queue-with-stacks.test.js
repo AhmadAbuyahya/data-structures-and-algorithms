e222aa106e0c1e61cd0bcbaf2577072b5f35ada4
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-05-25 19:13:49 +0300

    pseudo q

---
+++ javascript/QueueWithStacks/queue-with-stacks.test.js
@@ -0,0 +1,17 @@
+const Stack = require('../stacks-and-queues.js/stack');
+const PseudoQueue = require('./queue-with-stacks');
+
+describe('PseudoQueue', () => {
+  it('successfully enqueues values', () => {
+    let pq = new PseudoQueue();
+    pq.enqueue(1);
+    expect(pq.stack1.top).toEqual(1);
+  });
+  it('successfully dequeues values', () => {
+    let pq = new PseudoQueue();
+    pq.enqueue(1);
+    pq.enqueue(2);
+    pq.enqueue(3);
+    expect(pq.dequeue()).toEqual(1);
+  });
+})
